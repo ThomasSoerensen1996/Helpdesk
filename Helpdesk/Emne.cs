87a50894f921b9a2205fff285b835b08628c0b9e
alex5955 <43375012+alex5955@users.noreply.github.com>
2018-11-22 10:50:58 +0100

    Initial commit

---
+++ Helpdesk/Emne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpdesk
+{
+    class Emne
+    {
+        private string Titel { get; set; }
+        private string Beskrivelse { get; set; }
+        private string Forklaring { get; set; }
+        public string KodeEksempel { get; set; }
+        private string Semester { get; set; }
+        private Quizz Quizz { get; set; }
+
+        public Emne()
+        {
+            
+        }
+    }
+}
---
+++ Helpdesk/Quizz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpdesk
+{
+    class Quizz
+    {
+        //Q = Question
+        //Qa = Question answer
+
+        //Måske skal det laves på en anden måde? TEMP
+        public string Q1 { get; set; }
+        public string Qa1 { get; set; }
+        public string Q2 { get; set; }
+        public string Qa2 { get; set; }
+        public string Q3 { get; set; }
+        public string Qa3 { get; set; }
+
+        public Quizz()
+        {
+            
+        }
+    }
+}
