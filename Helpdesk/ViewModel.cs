2b710768b66b23c80c34a9ba710a26d3f67424d7
Resul134 <resulkurrr134@gmail.com>
2018-11-22 13:14:59 +0100

    Navigaton
    Navigaton via login, implementeret.

--- Helpdesk/ViewModel.cs
+++ Helpdesk/ViewModel.cs
@@ -16,6 +16,7 @@ namespace Helpdesk
 
         public ViewModel()
         {
+            
             EmneListe.Add(new Emne() { Titel = "Test" });
             EmneListe.Add(new Emne() { Titel = "Test2" });
             EmneListe.Add(new Emne() { Titel = "Test3" });
@@ -34,5 +35,35 @@ namespace Helpdesk
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        //Navigation til en andet page, via login.
+
+        //public void checkLogin()
+        //{
+        //    foreach (var user in _users)
+        //    {
+        //        if (InputUsername == user.Username)
+        //        {
+        //            if (InputPassword == user.Password)
+        //            {
+        //                LoginCheck = "Access granted";
+        //                Frame currentFrame = Window.Current.Content as Frame;
+        //                currentFrame.Navigate(typeof(BlankPage2));
+        //                break;
+        //            }
+
+        //            else
+        //            {
+        //                LoginCheck = "Wrong password";
+        //                break;
+        //            }
+
+
+
+        //        }
+        //        else LoginCheck = "Access denied";
+
+        //    }
+        //}
     }
 }
