80f81fe1f480f4d2dc3fc734083fca3e21a2f19c
GretheSlushice <43374884+GretheSlushice@users.noreply.github.com>
2018-11-23 09:53:12 +0100

    Revert "Emne constructor"
    This reverts commit c2eae911a1e4b9e27ddd1c129c889a84eda1f3dc.

--- Helpdesk/Emne.cs
+++ Helpdesk/Emne.cs
@@ -8,57 +8,16 @@ namespace Helpdesk
 {
     class Emne
     {
-        private string _titel;
-        private string _beskrivelse;
-        private string _forklaring;
-        private string _kodeEksempel;
-        private string _semester;
-        private Quiz Quiz;
+        public string Titel { get; set; }
+        private string Beskrivelse { get; set; }
+        private string Forklaring { get; set; }
+        private string KodeEksempel { get; set; }
+        private string Semester { get; set; }
+        private Quiz Quiz { get; set; }
 
-        public Emne(string _titel, string _beskrivelse, string _forklaring, string _kodeEksempel, string _semester)
+        public Emne()
         {
-            this._titel = Titel;
-            this._beskrivelse = Beskrivelse;
-            this._forklaring = Forklaring;
-            this._kodeEksempel = KodeEksempel;
-            this._semester = Semester;
-
-        }
-
-        public string Beskrivelse
-        {
-            get { return _beskrivelse; }
-            set { _beskrivelse = value; }
-        }
-
-        public string Forklaring
-        {
-            get { return _forklaring; }
-            set { _forklaring = value; }
-        }
-
-        public string KodeEksempel
-        {
-            get { return _kodeEksempel; }
-            set { _kodeEksempel = value; }
-        }
-
-        public string Semester
-        {
-            get { return _semester; }
-            set { _semester = value; }
-        }
-
-        public Quiz Quiz1
-        {
-            get { return Quiz; }
-            set { Quiz = value; }
-        }
-
-        public string Titel
-        {
-            get { return _titel; }
-            set { Titel = value;  }
+            
         }
     }
 }
