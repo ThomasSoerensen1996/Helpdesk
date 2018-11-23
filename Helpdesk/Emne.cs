6da46657e6c768aba846958251349093273a8716
alex5955 <43375012+alex5955@users.noreply.github.com>
2018-11-23 10:04:30 +0100

    fix?

--- Helpdesk/Emne.cs
+++ Helpdesk/Emne.cs
@@ -14,59 +14,5 @@ namespace Helpdesk
         private string KodeEksempel { get; set; }
         private string Semester { get; set; }
         private Quiz Quiz { get; set; }
-
-        public Emne()
-        private string _titel;
-        private string _beskrivelse;
-        private string _forklaring;
-        private string _kodeEksempel;
-        private string _semester;
-        private Quiz Quiz;
-
-        public Emne(string _titel, string _beskrivelse, string _forklaring, string _kodeEksempel, string _semester)
-        {
-            this._titel = Titel;
-            this._beskrivelse = Beskrivelse;
-            this._forklaring = Forklaring;
-            this._kodeEksempel = KodeEksempel;
-            this._semester = Semester;
-
-        }
-
-        public string Beskrivelse
-        {
-            get { return _beskrivelse; }
-            set { _beskrivelse = value; }
-        }
-
-        public string Forklaring
-        {
-            get { return _forklaring; }
-            set { _forklaring = value; }
-        }
-
-        public string KodeEksempel
-        {
-            get { return _kodeEksempel; }
-            set { _kodeEksempel = value; }
-        }
-
-        public string Semester
-        {
-            get { return _semester; }
-            set { _semester = value; }
-        }
-
-        public Quiz Quiz1
-        {
-            get { return Quiz; }
-            set { Quiz = value; }
-        }
-
-        public string Titel
-        {
-            get { return _titel; }
-            set { Titel = value;  }
-        }
     }
 }
