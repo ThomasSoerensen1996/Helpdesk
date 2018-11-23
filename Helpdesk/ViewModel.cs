548a075bec324c769832d94502508af965609b84
GretheSlushice <43374884+GretheSlushice@users.noreply.github.com>
2018-11-23 09:55:36 +0100

    Revert "Navigaton"
    This reverts commit 2b710768b66b23c80c34a9ba710a26d3f67424d7.

--- Helpdesk/ViewModel.cs
+++ Helpdesk/ViewModel.cs
@@ -16,7 +16,6 @@ namespace Helpdesk
 
         public ViewModel()
         {
-            
             EmneListe.Add(new Emne() { Titel = "Test" });
             EmneListe.Add(new Emne() { Titel = "Test2" });
             EmneListe.Add(new Emne() { Titel = "Test3" });
@@ -35,35 +34,5 @@ namespace Helpdesk
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-        //Navigation til en andet page, via login.
-
-        //public void checkLogin()
-        //{
-        //    foreach (var user in _users)
-        //    {
-        //        if (InputUsername == user.Username)
-        //        {
-        //            if (InputPassword == user.Password)
-        //            {
-        //                LoginCheck = "Access granted";
-        //                Frame currentFrame = Window.Current.Content as Frame;
-        //                currentFrame.Navigate(typeof(BlankPage2));
-        //                break;
-        //            }
-
-        //            else
-        //            {
-        //                LoginCheck = "Wrong password";
-        //                break;
-        //            }
-
-
-
-        //        }
-        //        else LoginCheck = "Access denied";
-
-        //    }
-        //}
     }
 }
