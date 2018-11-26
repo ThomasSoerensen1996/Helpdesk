2f52bb52673b00c1265a9b2ace19d2ed0fb268a1
Resul134 <resulkurrr134@gmail.com>
2018-11-26 11:57:19 +0100

    Done mangler observable collection

--- Helpdesk/PersistencyService.cs
+++ Helpdesk/PersistencyService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
+using Windows.Devices.AllJoyn;
 using Windows.Storage;
 using Windows.UI.Popups;
 using Newtonsoft.Json;
@@ -10,7 +11,65 @@ namespace Helpdesk
 {
     class PersistencyService
     {
-       private static string 
+
+        private static string _jsonFileName = "Notes.json";
+
+        public static string JsonFileName
+        {
+            get { return _jsonFileName; }
+            set { _jsonFileName = value; }
+        }
+
+        public static async void SaveNotesAsJsonAsync(ObservableCollection<Note> notes)
+        {
+            string notesJsonString = JsonConvert.SerializeObject(notes);
+            //SerializeNotesFileAsync(notesJsonString, JsonFileName);
+        }
+
+        public static async Task<List<Note>> LoadNotesFromJsonAsync()
+        {
+            string notesJsonString = await DeserializeNotesFileAsync(JsonFileName);
+            if (notesJsonString != null)
+            {
+                return (List<Note>) JsonConvert.DeserializeObject(notesJsonString, typeof(List<Note>));
+                return null;
+            }
+        }
+
+        private static async void SerializeNotesFileAsync(string notesJsonString, string filename)
+        {
+            StorageFile localFile =
+                await ApplicationData.Current.LocalFolder.CreateFileAsync(filename,
+                    CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(localFile, JsonFileName);
+        }
+
+        public static async Task<string> DeserializeNotesFileAsync(string fileName)
+        {
+            try
+            {
+                StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+                return await FileIO.ReadTextAsync(localFile);
+            }
+            catch (FileNotFoundException e)
+            {
+                MessageDialogHelper.Show("Loading for the first time? - Try Add and Save some Notes before trying to Save for the first time", "File not Found");
+                return null;
+                
+            }
+        }
+
+        private class MessageDialogHelper
+        {
+            public static async void Show(string content, string title)
+            {
+                MessageDialog messageDialog = new MessageDialog(content, title);
+                await messageDialog.ShowAsync();
+
+            }
+        }
+
+
 
     }
 }
